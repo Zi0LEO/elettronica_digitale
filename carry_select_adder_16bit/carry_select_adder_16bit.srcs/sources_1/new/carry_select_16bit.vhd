33301bd4a6a68706680c523e40accf4fb2bc0276
umbertofrega <162986073+umbertofrega@users.noreply.github.com>
2024-10-29 16:42:51 +0100

    completed implementation

--- carry_select_adder_16bit/carry_select_adder_16bit.srcs/sources_1/new/carry_select_16bit.vhd
+++ carry_select_adder_16bit/carry_select_adder_16bit.srcs/sources_1/new/carry_select_16bit.vhd
@@ -33,73 +33,105 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity carry_select_16bit is
     Port ( A : in STD_LOGIC_VECTOR (15 downto 0);
-           B : in STD_LOGIC_VECTOR (15 downto 0);
-           sum : out STD_LOGIC_VECTOR (15 downto 0));
+         B : in STD_LOGIC_VECTOR (15 downto 0);
+         sum : out STD_LOGIC_VECTOR (15 downto 0);
+         carry_in_start: in STD_LOGIC);
 end carry_select_16bit;
 
 architecture Behavioral of carry_select_16bit is
 
-component ripplecarry_4bit is
-    Port ( A_4 : in STD_LOGIC_VECTOR (3 downto 0);
-           B_4 : in STD_LOGIC_VECTOR (3 downto 0);
-           carry_in : in STD_LOGIC;
-           carry_out : out STD_LOGIC;
-           sum_4 : out STD_LOGIC_VECTOR (3 downto 0));
-end component;
+    component ripplecarry_4bit is
+        Port ( A_4 : in STD_LOGIC_VECTOR (3 downto 0);
+             B_4 : in STD_LOGIC_VECTOR (3 downto 0);
+             carry_in : in STD_LOGIC;
+             carry_out : out STD_LOGIC;
+             sum_4 : out STD_LOGIC_VECTOR (3 downto 0));
+    end component;
 
-shared variable carry_selector: STD_LOGIC;
-signal carry_start: STD_LOGIC;
-signal carry0, carry1: STD_LOGIC_VECTOR(2 downto 0);
-signal sum_0, sum_1: STD_LOGIC_VECTOR(12 downto 0);
+    shared variable carry_selector: STD_LOGIC;
+    signal carry_start: STD_LOGIC;
+    signal carry0, carry1: STD_LOGIC_VECTOR(2 downto 0);
+    signal sum_0, sum_1: STD_LOGIC_VECTOR(12 downto 0);
 
 begin
 
-ripplecarry0_0: ripplecarry_4bit PORT MAP (
-  A_4 => A(3 downto 0),
-  B_4 => B(3 downto 0), 
-  carry_in => '0', 
-  carry_out => carry_start, 
-  sum_4 => sum(3 downto 0));
+    ripplecarry0_0: ripplecarry_4bit PORT MAP (
+            A_4 => A(3 downto 0),
+            B_4 => B(3 downto 0),
+            carry_in => carry_in_start,
+            carry_out => carry_start,
+            sum_4 => sum(3 downto 0));
 
-ripplecarry1_0: ripplecarry_4bit PORT MAP (A(7 downto 4), B(7 downto 4), '0', carry0(0), sum_0(3 downto 0));
-ripplecarry1_1: ripplecarry_4bit PORT MAP (A(7 downto 4), B(7 downto 4), '1', carry1(0), sum_1(3 downto 0));
+    ripplecarry1_0: ripplecarry_4bit PORT MAP (
+            A_4 => A(7 downto 4),
+            B_4 => B(7 downto 4),
+            carry_in => '0',
+            carry_out => carry0(0),
+            sum_4 => sum_0(3 downto 0));
+    ripplecarry1_1: ripplecarry_4bit PORT MAP (
+            A_4 => A(7 downto 4),
+            B_4 => B(7 downto 4),
+            carry_in => '1',
+            carry_out => carry1(0),
+            sum_4 => sum_1(3 downto 0));
 
-ripplecarry2_0: ripplecarry_4bit PORT MAP (A(11 downto 8), B(11 downto 8), '0', carry0(1), sum_0(7 downto 4));
-ripplecarry2_1: ripplecarry_4bit PORT MAP (A(11 downto 8), B(11 downto 8), '1', carry1(1), sum_1(7 downto 4));
+    ripplecarry2_0: ripplecarry_4bit PORT MAP (
+            A_4 => A(11 downto 8),
+            B_4 => B(11 downto 8),
+            carry_in => '0',
+            carry_out => carry0(1),
+            sum_4 => sum_0(7 downto 4));
+    ripplecarry2_1: ripplecarry_4bit PORT MAP (
+            A_4 => A(7 downto 4),
+            B_4 => B(7 downto 4),
+            carry_in => '1',
+            carry_out => carry1(1),
+            sum_4 => sum_1(7 downto 4));
 
-ripplecarry3_0: ripplecarry_4bit PORT MAP (A(15 downto 12), B(15 downto 12), '0', carry0(2), sum_0(11 downto 8));
-ripplecarry3_1: ripplecarry_4bit PORT MAP (A(15 downto 12), B(15 downto 12), '1', carry1(2), sum_1(11 downto 8));
+    ripplecarry3_0: ripplecarry_4bit PORT MAP (
+            A_4 => A(15 downto 12),
+            B_4 => B(15 downto 12),
+            carry_in => '0',
+            carry_out => carry0(2),
+            sum_4 => sum_0(11 downto 8));
+    ripplecarry3_1: ripplecarry_4bit PORT MAP (
+            A_4 => A(15 downto 12),
+            B_4 => B(15 downto 12),
+            carry_in => '1',
+            carry_out => carry1(2),
+            sum_4 => sum_1(11 downto 8));
 
-process(carry_start, sum_0, sum_1, carry_start) begin
 
-  case carry_start is
-    when '0' => 
-      sum( 7 downto 4 ) <= sum_0(3 downto 0);
-      carry_selector := carry0(0);
-    when  others => 
-      sum( 7 downto 4 ) <= sum_1(3 downto 0);
-        carry_selector := carry1(0);
-    end case;
-    
-    case carry_selector is
-      when '0' => 
-      sum( 11 downto 8 ) <= sum_0(7 downto 4);
-      carry_selector := carry0(1);
-    when  others => 
-      sum( 11 downto 8 ) <= sum_1(7 downto 4);
-        carry_selector := carry1(1);
-    end case;
-    
+    process(carry_start, sum_0, sum_1, carry_start) begin
+
+        case carry_start is
+            when '0' =>
+                sum( 7 downto 4 ) <= sum_0(3 downto 0);
+                carry_selector := carry0(0);
+            when  others =>
+                sum( 7 downto 4 ) <= sum_1(3 downto 0);
+                carry_selector := carry1(0);
+        end case;
+
         case carry_selector is
-      when '0' => 
-      sum( 15 downto 12 ) <= sum_0(11 downto 8);
-      carry_selector := carry0(2);
-    when  others => 
-      sum( 15 downto 12 ) <= sum_1(11 downto 8);
-        carry_selector := carry1(2);
-    end case;
-    
-  end process;
-  
-  
+            when '0' =>
+                sum( 11 downto 8 ) <= sum_0(7 downto 4);
+                carry_selector := carry0(1);
+            when  others =>
+                sum( 11 downto 8 ) <= sum_1(7 downto 4);
+                carry_selector := carry1(1);
+        end case;
+
+        case carry_selector is
+            when '0' =>
+                sum( 15 downto 12 ) <= sum_0(11 downto 8);
+                carry_selector := carry0(2);
+            when  others =>
+                sum( 15 downto 12 ) <= sum_1(11 downto 8);
+                carry_selector := carry1(2);
+        end case;
+
+    end process;
+
+
 end Behavioral;
--- carry_select_adder_16bit/carry_select_adder_16bit.srcs/sources_1/new/ripplecarry_4bit.vhd
+++ carry_select_adder_16bit/carry_select_adder_16bit.srcs/sources_1/new/ripplecarry_4bit.vhd
@@ -33,30 +33,29 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity ripplecarry_4bit is
     Port ( A_4 : in STD_LOGIC_VECTOR (3 downto 0);
-           B_4 : in STD_LOGIC_VECTOR (3 downto 0);
-           carry_in : in STD_LOGIC;
-           carry_out : out STD_LOGIC;
-           sum_4 : out STD_LOGIC_VECTOR (3 downto 0));
+         B_4 : in STD_LOGIC_VECTOR (3 downto 0);
+         carry_in : in STD_LOGIC;
+         carry_out : out STD_LOGIC;
+         sum_4 : out STD_LOGIC_VECTOR (3 downto 0));
 end ripplecarry_4bit;
 
 architecture Behavioral of ripplecarry_4bit is
 
-component fulladder_1bit is
-    Port ( A : in STD_LOGIC;
-           B : in STD_LOGIC;
-           carry_in : in STD_LOGIC;
-           carry_out : out STD_LOGIC;
-           sum : out STD_LOGIC);
-end component;
+    component fulladder_1bit is
+        Port ( A : in STD_LOGIC;
+             B : in STD_LOGIC;
+             carry_in : in STD_LOGIC;
+             carry_out : out STD_LOGIC;
+             sum : out STD_LOGIC);
+    end component;
 
-signal sum, carry: STD_LOGIC_VECTOR(3 downto 0);
-
+    signal sum, carry: STD_LOGIC_VECTOR(3 downto 0);
 begin
-
-fa0:fulladder_1bit PORT MAP( A_4(0), B_4(0), '0', carry(0), sum(0) );
-fa1:fulladder_1bit PORT MAP( A_4(1), B_4(1), carry(0), carry(1), sum(0) );
-fa2:fulladder_1bit PORT MAP( A_4(2), B_4(2), carry(1), carry(2), sum(0) );
-fa3:fulladder_1bit PORT MAP( A_4(3), B_4(3), carry(2), carry(3), sum(0) );
-
+    fa0: fulladder_1bit PORT MAP( A_4(0), B_4(0), carry_in, carry(0), sum(0) );
+    fa1: fulladder_1bit PORT MAP( A_4(1), B_4(1), carry(0), carry(1), sum(1) );
+    fa2: fulladder_1bit PORT MAP( A_4(2), B_4(2), carry(1), carry(2), sum(2) );
+    fa3: fulladder_1bit PORT MAP( A_4(3), B_4(3), carry(2), carry(3), sum(3) );
+    sum_4 <= sum;
+    carry_out <= carry(3);
 
 end Behavioral;
