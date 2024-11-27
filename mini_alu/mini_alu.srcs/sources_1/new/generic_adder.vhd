2cb4d7e4e4b5c8c6a5e271dfd3f02b3f82aefad7
thecoder <leonardo.npl.02@gmail.com>
2024-11-27 15:24:49 +0100

    changed logic implementation to avoid using unsigned

--- mini_alu/mini_alu.srcs/sources_1/new/generic_adder.vhd
+++ mini_alu/mini_alu.srcs/sources_1/new/generic_adder.vhd
@@ -3,23 +3,23 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity generic_adder is
-    generic (n : INTEGER := 4);
-    Port ( A_adder,B_adder: in STD_LOGIC_VECTOR (n-1 downto 0);
+    generic (bit_number : INTEGER := 4);
+    Port ( A_adder,B_adder: in STD_LOGIC_VECTOR (bit_number-1 downto 0);
          cin : in STD_LOGIC;
-         sum : out STD_LOGIC_VECTOR (n downto 0));
+         sum : out STD_LOGIC_VECTOR (bit_number downto 0));
 end generic_adder;
 
 architecture Behavioral of generic_adder is
-    signal p,g : STD_LOGIC_VECTOR (n downto 0);
-    signal carry : STD_LOGIC_VECTOR (n+1 downto 0);
+    signal p,g : STD_LOGIC_VECTOR (bit_number downto 0);
+    signal carry : STD_LOGIC_VECTOR (bit_number+1 downto 0);
 begin
     carry(0) <= cin;
-    p_g: for i in 0 to n generate
-        p_gMSB: if (i=n) generate
-            p(i) <= A_adder(n-1) xor B_adder(n-1);
-            g(i) <= A_adder(n-1) and B_adder(n-1);
+    p_g: for i in 0 to bit_number generate
+        p_gMSB: if (i=bit_number) generate
+            p(i) <= A_adder(bit_number-1) xor B_adder(bit_number-1);
+            g(i) <= A_adder(bit_number-1) and B_adder(bit_number-1);
         end generate;
-        p_gLSB: if i<n generate
+        p_gLSB: if i<bit_number generate
             p(i) <= A_adder(i) xor B_adder(i);
             g(i) <= A_adder(i) and B_adder(i);
         end generate;
--- mini_alu/mini_alu.srcs/sources_1/new/mini_alu.vhd
+++ mini_alu/mini_alu.srcs/sources_1/new/mini_alu.vhd
@@ -3,22 +3,23 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity mini_alu is
-  generic (n : INTEGER := 4);
-    Port ( A,B : in STD_LOGIC_VECTOR (n-1 downto 0);
+  generic (bit_number : INTEGER := 4);
+    Port ( A,B : in STD_LOGIC_VECTOR (bit_number-1 downto 0);
            C : in STD_LOGIC;
-           output : out STD_LOGIC_VECTOR (n downto 0));
+           output : out STD_LOGIC_VECTOR (bit_number downto 0));
 end mini_alu;
 
 architecture Behavioral of mini_alu is
   component generic_adder is
-    generic (n:INTEGER := 4);
+    generic (bit_number:INTEGER := 4);
       Port ( 
-        A_adder, B_adder : in STD_LOGIC_VECTOR (n-1 downto 0);
+        A_adder, B_adder : in STD_LOGIC_VECTOR (bit_number-1 downto 0);
         cin : in STD_LOGIC;
-        sum : out STD_LOGIC_VECTOR (n downto 0));
+        sum : out STD_LOGIC_VECTOR (bit_number downto 0));
   end component;
 
-signal B_internal: STD_LOGIC_VECTOR (n-1 downto 0);
+signal B_internal: STD_LOGIC_VECTOR (bit_number-1 downto 0);
+signal carry_in: STD_LOGIC;
 
 begin
       
@@ -26,17 +27,19 @@ begin
     case C is
       when '0' => 
       B_internal <= B;
+      carry_in <= '0';
       
       when others =>
       B_internal <= STD_LOGIC_VECTOR(UNSIGNED(not B) + "1");
+      carry_in <= '1';
     end case;
   end process;
 
 generic_adder_alu: generic_adder 
-      GENERIC MAP (n => n)
+      GENERIC MAP (bit_number => bit_number)
       PORT MAP (
       A_adder => A,
       B_adder => B_internal,
-      cin => '0',
+      cin => carry_in,
       sum => output);
 end Behavioral;
