74b47c6e7452f3e7fc76149b742e3ec081bdb6f0
umbertofrega <umbertofrega@gmail.com>
2024-12-26 17:20:35 +0100

    further implementation

--- moltiplicatore_binario/moltiplicatore_binario.srcs/sources_1/new/carry_save_adder.vhd
+++ moltiplicatore_binario/moltiplicatore_binario.srcs/sources_1/new/carry_save_adder.vhd
@@ -4,30 +4,30 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity carry_save_adder is
  generic (nbit : INTEGER := 16);
      Port ( A,B,C : in STD_LOGIC_VECTOR (nbit-1 downto 0);
-           sum : out STD_LOGIC_VECTOR (nbit+1 downto 0));
+           sum : out STD_LOGIC_VECTOR (nbit downto 0));
 end carry_save_adder;
     
 architecture Behavioral of carry_save_adder is
     component generic_adder 
-        generic (nbit_adder : INTEGER := nbit+1);
+        generic (bit_number : INTEGER := nbit);
           Port ( 
-            A_adder, B_adder : in STD_LOGIC_VECTOR (nbit_adder-1 downto 0);
-            sum_adder : out STD_LOGIC_VECTOR (nbit_adder downto 0));
+            A_adder, B_adder : in STD_LOGIC_VECTOR (bit_number downto 0);
+            sum_adder : out STD_LOGIC_VECTOR (bit_number downto 0));
     end component;
         
     signal sub_sum,carry : STD_LOGIC_VECTOR (nbit-1 downto 0);
-    signal sub_sum_ext,carry_shft : STD_LOGIC_VECTOR (nbit downto 0);
+    signal sub_sum_ext,carry_ext : STD_LOGIC_VECTOR (nbit downto 0);
         
    begin
-    sub_sum  <= (A xor B) xor C;
+    sub_sum  <= A xor B xor C;
     carry <= (A and B) or (B and C) or (C and A);
-    carry_shft <= carry & "0";
-    sub_sum_ext <= sub_sum(nbit-1) & sub_sum;
+    carry_ext <= carry & '0';
+    sub_sum_ext <= '1' & sub_sum;
         
     adder : generic_adder
-      GENERIC MAP (nbit+1)
+      GENERIC MAP (nbit)
       PORT MAP (
-        A_adder => carry_shft,
+        A_adder => carry_ext,
         B_adder => sub_sum_ext,
         sum_adder => sum );
         
--- moltiplicatore_binario/moltiplicatore_binario.srcs/sources_1/new/generic_adder.vhd
+++ moltiplicatore_binario/moltiplicatore_binario.srcs/sources_1/new/generic_adder.vhd
@@ -2,26 +2,35 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity generic_adder is
-    generic (bit_number : INTEGER);
-    Port ( A_adder,B_adder: in STD_LOGIC_VECTOR (bit_number-1 downto 0);
-         sum : out STD_LOGIC_VECTOR (bit_number downto 0));
+    generic (bit_number : INTEGER := 16);
+    Port ( A_adder,B_adder: in STD_LOGIC_VECTOR (bit_number downto 0);
+         sum_adder : out STD_LOGIC_VECTOR (bit_number downto 0));
 end generic_adder;
 
 architecture Behavioral of generic_adder is
-    signal p,g : STD_LOGIC_VECTOR (bit_number downto 0);
-    signal carry : STD_LOGIC_VECTOR (bit_number+1 downto 0);
-begin
-    carry(0) <= '0';
-    p_g: for i in 0 to bit_number generate
-        p_gMSB: if (i=bit_number) generate
-            p(i) <= A_adder(bit_number-1) xor B_adder(bit_number-1);
-            g(i) <= A_adder(bit_number-1) and B_adder(bit_number-1);
-        end generate;
-        p_gLSB: if i<bit_number generate
-            p(i) <= A_adder(i) xor B_adder(i);
-            g(i) <= A_adder(i) and B_adder(i);
-        end generate;
-        carry(i+1) <= (g(i) or (p(i) and carry(i)));
-        sum(i) <= carry(i) xor p(i);
+component fulladder_1bit is
+        Port ( 
+             A : in STD_LOGIC;
+             B : in STD_LOGIC;
+             carry_in : in STD_LOGIC;
+             carry_out : out STD_LOGIC;
+             sum : out STD_LOGIC);
+    end component;
+    signal carry : STD_LOGIC_VECTOR( bit_number+1 downto 0 );
+begin  
+    carry (0) <= '0';
+    
+    gen_adder: for i in 0 to bit_number generate
+         fa: fulladder_1bit
+            PORT MAP(
+                A         => A_adder(i),
+                B         => B_adder(i),
+                carry_in  => carry(i),
+                carry_out => carry(i + 1),
+                sum       => sum_adder(i)
+            );
     end generate;
+    
+    sum_adder(bit_number) <= carry(bit_number);
+    
 end Behavioral;
