b5e10b85d42164f654de4cfb07a806362fcef125
umbertofrega <umbertofrega@gmail.com>
2025-01-04 19:25:22 +0100

    implemented adder tree

--- moltiplicatore_binario/moltiplicatore_binario.srcs/sources_1/new/full_adder.vhd
+++ moltiplicatore_binario/moltiplicatore_binario.srcs/sources_1/new/full_adder.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 10/28/2024 03:52:01 PM
--- Design Name: 
--- Module Name: fulladder_1bit - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
