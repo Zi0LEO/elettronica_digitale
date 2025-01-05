829c0d92cf4cea167b6297b831a0ec3eedda50ca
thecoder <leonardo.npl.02@gmail.com>
2025-01-05 15:18:02 +0100

    removed signals previously useful

--- moltiplicatore_binario/moltiplicatore_binario.srcs/sources_1/new/carry_save_adder.vhd
+++ moltiplicatore_binario/moltiplicatore_binario.srcs/sources_1/new/carry_save_adder.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
     
 entity carry_save_adder is
- generic (nbit_csa : INTEGER);
+ generic (nbit_csa : INTEGER := 16);
      Port ( 
        A,B,C: in STD_LOGIC_VECTOR (nbit_csa-1 downto 0);
        sum: out STD_LOGIC_VECTOR (nbit_csa downto 0);
@@ -12,7 +12,6 @@ end carry_save_adder;
 architecture Behavioral of carry_save_adder is
 
     signal sub_sum,sub_carry : STD_LOGIC_VECTOR (nbit_csa-1 downto 0);
-    signal sub_sum_ext,carry_ext : STD_LOGIC_VECTOR (nbit_csa downto 0);
         
   begin
     sub_sum  <= A xor B xor C;
