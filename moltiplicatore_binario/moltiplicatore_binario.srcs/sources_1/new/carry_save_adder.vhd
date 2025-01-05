ab1e954059b6b78854d41121a69a7fdc157f1a05
thecoder <leonardo.npl.02@gmail.com>
2025-01-05 11:49:22 +0100

    removed unused component

--- moltiplicatore_binario/moltiplicatore_binario.srcs/sources_1/new/carry_save_adder.vhd
+++ moltiplicatore_binario/moltiplicatore_binario.srcs/sources_1/new/carry_save_adder.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
     
 entity carry_save_adder is
- generic (nbit : INTEGER := 32);
+ generic (nbit : INTEGER);
      Port ( 
        A,B,C: in STD_LOGIC_VECTOR (nbit-1 downto 0);
        sum: out STD_LOGIC_VECTOR (nbit downto 0);
@@ -10,12 +10,6 @@ entity carry_save_adder is
 end carry_save_adder;
     
 architecture Behavioral of carry_save_adder is
-  component generic_adder 
-    generic (bit_number : INTEGER := nbit);
-    Port ( 
-      A_adder, B_adder : in STD_LOGIC_VECTOR (bit_number downto 0);
-      sum_adder : out STD_LOGIC_VECTOR (bit_number+1 downto 0));
-  end component;
 
     signal sub_sum,sub_carry : STD_LOGIC_VECTOR (nbit-1 downto 0);
     signal sub_sum_ext,carry_ext : STD_LOGIC_VECTOR (nbit downto 0);
@@ -23,7 +17,7 @@ architecture Behavioral of carry_save_adder is
   begin
     sub_sum  <= A xor B xor C;
     sub_carry <= (A and B) or (B and C) or (C and A);
-    carry <= carry & '0';
+    carry <= sub_carry & '0';
     sum <= '0' & sub_sum;
     
 end Behavioral;
