72432ef696b225331d66d1b3079ae7c524463c2d
thecoder <leonardo.npl.02@gmail.com>
2025-01-05 15:18:55 +0100

    fixed partial product logic

--- moltiplicatore_binario/moltiplicatore_binario.srcs/sources_1/new/moltiplicatore.vhd
+++ moltiplicatore_binario/moltiplicatore_binario.srcs/sources_1/new/moltiplicatore.vhd
@@ -1,6 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use work.AdderTree_Types.all;
 
 entity moltiplicatore is
     generic (nbit : INTEGER := 16);
@@ -10,68 +10,28 @@ entity moltiplicatore is
 end moltiplicatore;
 
 architecture Behavioral of moltiplicatore is
-  component carry_save_adder
-    generic (nbit2 : INTEGER := nbit*2);
-      Port (
-        A,B,C : in STD_LOGIC_VECTOR (nbit2-1 downto 0); --32 bit
-        sum : out STD_LOGIC_VECTOR (nbit2+1 downto 0)); --34bit, because it adds 3 numbers   
+  component adder_tree is
+    port ( p: MAT;
+            ris : out STD_LOGIC_VECTOR(nbit*2-1 downto 0));
   end component;
   
-    type MAT is array ( nbit+1 downto 0 ) of STD_LOGIC_VECTOR((nbit*2)-1 downto 0); --32bit
     signal p : MAT; --32bit
-    signal temp : STD_LOGIC_VECTOR(nbit-1 downto 0); --16bit
-    signal temp_sum : STD_LOGIC_VECTOR(nbit*2+1 downto 0); --32bit
   begin 
   -- Generate partial products
   outer: for i in 0 to nbit-1 generate
-    temp <= (others => '0'); --16bit
     inner: for j in 0 to nbit-1 generate
-      temp(j) <= A(i) and B(j);
+        -- Clear the higher part of p(i)
+        p(i)(nbit*2-2 downto nbit+i) <= (others => '0');
+        
+        -- Set the middle part of p(i) with the partial product A(i) and B(j)
+        p(i)(i+j) <= (A(i) and B(j));
+        
+        -- Clear the lower part of p(i)
+        p(i)(i downto 0) <= (others => '0');
     end generate inner;
-    --Shift to the left
-    p(i) <= (others => '0') when i = 0 else
-         ((nbit*2)-1 downto (nbit+i) => '0') & temp & (i-1 downto 0 => '0');
-  end generate outer;
+  end generate outer; 
+      
+  adder:adder_tree
+    PORT MAP(p, prod);
   
-  --Add them together
-  prod <= (others => '0');
-  add: for i in 0 to (nbit-1)/3 - 1 generate  --p(15) does not get computed
-    temp_sum <= (others => '0');
-   
-  --aggiungo gli ultimi due bit a p come tutti 0 e l'ultimo bit di B in mezzo
-  p(nbit) <= ((nbit/2 downto 0 => '0') & B(nbit-1)) & ((nbit/2)-1  downto 0 => '0');
-  --lo xor dell'ultimo prodotto parziale esteso con gli 0 alla fine
-  p(nbit+1) <= (B(nbit-1) xor (B(nbit-1) and A)) & (((nbit/2)-1  downto 0 => '0'));
-    
-  --l'adder tree avrà 18 ingressi quindi 
-    adder:carry_save_adder
-      PORT MAP(
-        A   => p(i*3),
-        B   => p(i*3+1),
-        C   => p(i*3+2),
-        sum => temp_sum);
-      p(i) <= temp_sum(nbit*2 -1 downto 0);
-    end generate add;
-
-  level2_adder: carry_save_adder --26bit in, 28bit out
-    PORT MAP(
-      A   => p(0)(nbit+9 downto 0),
-      B   => p(1)(nbit+9 downto 0),
-      C   => p(2)(nbit+9 downto 0),
-      sum => p(0));
-      
-  level2_adder2: carry_save_adder --20bit in, 22bit out
-    PORT MAP(
-      A   => p(3)(nbit+3 downto 0),
-      B   => p(4)(nbit+3 downto 0),
-      C   => p(15)(nbit*2-1 downto nbit-3),
-      sum => p(1));
-      
-  level3_adder: carry_save_adder --22bit in, 24bit out
-    PORT MAP(
-      A   => p(0)(nbit+1 downto 0),
-      B   => p(1)(nbit+1 downto 0),
-      C   => (others => '0'),
-      sum => prod);
-      
 end Behavioral;
